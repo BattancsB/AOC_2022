e9258c5cfc8e329d02319213bc49b96ec2cc8fd8
Benedek Szabó-Battancs <benedek.szabo-battancs@casrd.net>
2022-12-08 17:02:09 +0100

    DAY8 after uncountable tries🙃

--- Day_8/day8.ts
+++ Day_8/day8.ts
@@ -27,10 +27,10 @@ function read2DArray(lines: string[]): number[][] {
 function processArray(array: number[][]): number {
   const rowCount = array.length;
   const columnCount = array[0].length;
-  let counter = (rowCount-1) * (columnCount-1);
+  let counter = 2 * (rowCount) + 2 * (columnCount - 2);
 
-  for (let i = 1; i < rowCount-1; i++) {
-    for (let j = 1; j < columnCount-1; j++) {
+  for (let i = 1; i < rowCount - 1; i++) {
+    for (let j = 1; j < columnCount - 1; j++) {
       let flag1: boolean = false;
       let flag2: boolean = false;
       let flag3: boolean = false;
@@ -47,7 +47,7 @@ function processArray(array: number[][]): number {
         }
       })
       array[i].slice(j + 1, columnCount).forEach(x => {
-        right = array[i].slice(j + 1, columnCount + 2)
+        right = array[i].slice(j + 1, columnCount)
         if (x >= array[i][j]) {
           flag2 = true;
         }
@@ -94,8 +94,71 @@ function processArray(array: number[][]): number {
   }
   console.log(counter);
   return counter;
-  
-  
+
+
+}
+
+function processArray2(array: number[][]): number[] {
+  let result: number[] = [];
+  const rowCount = array.length;
+  const columnCount = array[0].length;
+  let counter = 2 * (rowCount) + 2 * (columnCount - 2);
+
+  for (let i = 1; i < rowCount - 1; i++) {
+    for (let j = 1; j < columnCount - 1; j++) {
+      let flag1: boolean = false;
+      let flag2: boolean = false;
+      let flag3: boolean = false;
+      let flag4: boolean = false;
+      let left: number[] = [];
+      let right: number[] = [];
+      let top: number[] = [];
+      let down: number[] = [];
+
+      let counter1: number = 0;
+      let counter2: number = 0;
+      let counter3: number = 0;
+      let counter4: number = 0;
+
+      array[i].slice(0, j).reverse().forEach(x => {
+        if (!flag1)
+          counter1++;
+
+        if (x >= array[i][j]) {
+          flag1 = true;
+        }
+      })
+      array[i].slice(j + 1, columnCount).forEach(x => {
+        if (!flag2) {
+          counter2++;
+          if (x >= array[i][j]) {
+            flag2 = true;
+          }
+        }
+      })
+
+      for (let h1 = i - 1; h1 >= 0; h1--) {
+        if (!flag3) {
+          counter3++;
+        }
+        if (array[h1][j] >= array[i][j]) {
+          flag3 = true
+        }
+      }
+
+      for (let h2 = i + 1; h2 < rowCount; h2++) {
+        if (!flag4) {
+          counter4++;
+        }
+        if (array[h2][j] >= array[i][j]) {
+          flag4 = true
+        }
+      }
+      result.push(counter1 * counter2 * counter3 * counter4);
+      console.log('elem: ' + array[i][j] + ' bal: ' + counter1 + ' jobb: ' + counter2 + ' top: ' + counter3 + ' down: ' + counter4)
+    }
+  }
+  return result;
 }
 
 
@@ -103,10 +166,20 @@ function processArray(array: number[][]): number {
 
 function part1() {
   const lines = readFl('input.txt', '\n')
-  
+
   processArray(read2DArray(lines))
-  
+
 }
 
-part1()
+function part2() {
+  const lines = readFl('input.txt', '\n')
+
+  const res = processArray2(read2DArray(lines));
+  res.sort((n1, n2) => n1 - n2)
+  res.reverse();
+  console.log('pisa' +
+    res[0]);
+}
+
+part2()
 
