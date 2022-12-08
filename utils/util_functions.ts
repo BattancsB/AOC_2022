b323657d1739cde133f723e6058c81f8db86d6e7
Benedek Szabó-Battancs <benedek.szabo-battancs@casrd.net>
2022-12-08 11:02:31 +0100

    Array print

--- utils/util_functions.ts
+++ utils/util_functions.ts
@@ -54,3 +54,12 @@ export function findIntersection(words: string[]): string {
   });
   return filteredArray[0];
 }
+
+export function print2DArray(arr: number[][]){
+  for(let i = 0; i < arr.length; i++){
+    for(let j = 0; j < arr[0].length; j++){
+      process.stdout.write(arr[i][j] +' ');
+    }
+    console.log('\n');
+  }
+}
